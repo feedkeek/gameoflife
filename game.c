98687b218f04dcb40f4f4f2bad6f6dbbf912fa5e
feedkeek <feedkeek@gmail.com>
2021-06-08 19:58:09 +0400

    Step 1 was done

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include "game.h"
 
+// allocate new board with given size
+// board is 2d array of cell's
 Cell* new_board(int size) {
     Cell* board = (Cell*)malloc(sizeof(Cell) * size * size);
     board -> size = size;
@@ -8,11 +10,11 @@ Cell* new_board(int size) {
     }
     return board;
 }
-
+// get cell at given row and column
 int get_cell(Cell* board, int r, int c) {
     return board[r + c * board->size].cell;
 }
-
+// set cell at given position to cellType
 void set_cell(Cell* board, int cellType, int r, int c) {
     board[r + c * board->size].cell = cellType;
 }
